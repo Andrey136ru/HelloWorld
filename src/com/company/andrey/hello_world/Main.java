bf52fddfa6834a2cd0ab6ee2d97c22edae4c690c
Andrey <andrey136ru@mail.ru>
2021-07-12 10:54:43 +0300

    Initial commit

---
+++ src/com/company/andrey/hello_world/Main.java
@@ -0,0 +1,9 @@
+package com.company.andrey.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+        System.out.println();
+    }
+}
